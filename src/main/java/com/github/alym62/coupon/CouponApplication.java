62946dd7113daf6a59ee28b3f5ad0b46f415ecd7
Alyasaf Meireles <111710522+Alym62@users.noreply.github.com>
2026-05-12 21:28:57 -0300

    feat: estrutura inicial do projeto

---
+++ src/main/java/com/github/alym62/coupon/CouponApplication.java
@@ -0,0 +1,13 @@
+package com.github.alym62.coupon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CouponApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CouponApplication.class, args);
+	}
+
+}
